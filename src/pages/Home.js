05be8bd227e9d43031b6cc45dbf36d17895f9dbc
Anton Thunberg <anton@thunberg.nu>
2022-09-26 12:57:10 +0200

    first

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+import Main from '../components/Main';
+
+export default function Home() {
+    return (
+        <div>
+            
+            <Main />
+            
+        </div>
+    )
+}
